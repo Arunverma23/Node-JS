f1f23f15af3a1ba0b000e0e622798d1c8b816335
Arun Verma <info.arunverma23@gmail.com>
2025-11-19 11:49:03 +0530

    Update 00_basics.js

--- 00_basics.js
+++ 00_basics.js
@@ -1,9 +1,66 @@
+
+// Keywords used to decalre variables
+
 let a = 13;
 var b = 87;
 const c = a+b;
 
 console.log(a+b,c);
 
+// if statement
+
 if(a == 200){
-    console.log("this is if condition");
+    console.log("This is if condition");
 }
+
+// Function
+
+function developer(category){
+    console.log(`I am ${category} Developer`);
+}
+
+developer('Backend');
+developer('Frontend');
+
+// For Loop 
+
+for(var i=0;i<=10;i++){
+    console.log(i);
+}
+
+// While Loop
+
+while(i<=20){
+    console.log(i);
+    i++;
+}
+
+// Do While Loop
+
+do {
+    console.log("I am Do While Loop");
+} while (false);
+
+// Array
+
+var users = ["Arun","Alex","Elon","Bruce"];
+console.log(users[0]);
+
+for(var user=0;user<users.length;user++){
+    console.log(`I am the ${user}th ${users[user]}`);
+}
+
+// Object
+
+var user_details = {
+    name : {
+        full_name : 'Arun',
+        last_name : 'Verma'
+    },
+    Age : '21'
+}
+
+console.log(`My Full Name is ${user_details.name.full_name + ` ${user_details.name.last_name}`}
+I am ${user_details.Age}`);
+
+
