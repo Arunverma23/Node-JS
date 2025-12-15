411cb0c920080432b3383cccf7a6fabbcd95dc20
Arun Verma <info.arunverma23@gmail.com>
2025-12-15 14:53:34 +0530

    path and global Constants

---
+++ 01_server/13_path.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const file = "text/Arun.txt"
+
+// console.log(path.extname(file));
+// console.log(path.dirname(file));
+// console.log(path.basename(file));
+
+// console.log(path.resolve("text","Arun.txt"));
+// console.log(path.resolve(path.dirname(file), path.basename(file)));
+// console.log(path.isAbsolute(file));
+
+// Global Constant
+console.log(__dirname);
+console.log(__filename);
